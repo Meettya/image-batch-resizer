0efc3b838dfaa193c00651090c408c96269c13dc
Karpich Dmitry <karpich@gollard.ru>
2017-04-11 17:55:49 +0300

    * rewrite config

---
+++ etc/theme.js
@@ -0,0 +1,20 @@
+/*
+ * Colors scheme for console
+ */
+
+module.exports = {
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'magenta',
+  out: 'yellow',
+  debug: 'blue',
+  error: 'red',
+  fileIn: 'cyan',
+  fileOut: 'yellow',
+  fileEqual: 'grey',
+  fileDelete: 'magenta'
+};
